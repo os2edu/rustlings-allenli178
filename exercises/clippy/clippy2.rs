2ff0e7947076837a0826f7ac00d9ad8c5e3cd76f
YuYan Li <allenli178@qq.com>
2022-11-04 22:05:57 +0800

    clippy2 过不去😢

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -2,7 +2,7 @@
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a hint.
 
 fn main() {
-    let mut res = 41;
+    let mut res = 14;
     let option = Some(12);
     while let Some(x) = option {
         res += x;
